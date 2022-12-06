eedd93fcad055b5df37116f7b8b5ffe6a220e424
Cucumber0078 <ashmore0757@gmail.com>
2022-12-06 20:41:18 +0000

    Homework_Programme1

---
+++ src/test/java/com/orangehrmlive/opensourcedemo/testsuite/LoginPageTest.java
@@ -0,0 +1,21 @@
+package com.orangehrmlive.opensourcedemo.testsuite;
+import com.orangehrmlive.opensourcedemo.pages.LoginPage;
+import com.orangehrmlive.opensourcedemo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends BaseTest {
+    LoginPage loginPage = new LoginPage();
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials() throws InterruptedException {
+      Thread.sleep(5000);
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickOnLoginButton();
+        String expectedMessage  = "Dashboard";
+        Assert.assertEquals(loginPage.getHeaterTitleDashboard(),expectedMessage,"Login page not displayed");
+    }
+
+
+    }
+
